a2873483a7384d9da11b3a758cd0a5ce7e01a808
beiciliang <liangbeici@gmail.com>
2019-03-18 12:20:18 +0000

    add files

---
+++ hello-world-in-japanese.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+print("こんにちは世界!")
---
+++ hello-world.py
@@ -0,0 +1,3 @@
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+print("Hello world!")
